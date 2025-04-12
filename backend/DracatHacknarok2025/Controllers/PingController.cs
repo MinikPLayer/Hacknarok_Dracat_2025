63f2062546aa5f2f12240f096c5ee7f3f6e6d5be
Michał Tomecki <minexplaypl@gmail.com>
2025-04-12 13:27:29 +0200

    Added the Ping backend.

---
+++ backend/DracatHacknarok2025/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DracatHacknarok2025.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string? data)
+        {
+            data ??= "Pong!";
+            return Ok(data);
+        }
+    }
+}
