6767f6925c9f109336cb17ca0779f065950e8989
Michał Tomecki <minexplaypl@gmail.com>
2025-04-12 21:09:09 +0200

    Added multiple points route.

--- frontend/src/components/mapWithPins/mapWithPins.js
+++ frontend/src/components/mapWithPins/mapWithPins.js
@@ -9,15 +9,14 @@ import DirectionsIcon from '@mui/icons-material/Directions';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = DefaultIcon;
+// let DefaultIcon = L.DivIcon({
+//     html: '<div style="font-size: 24px; color: #1976d2;">❓</div>',
+//     iconSize: [25, 41],
+//     iconAnchor: [12, 41],
+//     popupAnchor: [1, -34],
+//     shadowSize: [41, 41]
+// });
+// L.Marker.prototype.options.icon = DefaultIcon;
 
 // Custom icons
 const createQuestionIcon = () => new L.DivIcon({
@@ -28,8 +27,25 @@ const createQuestionIcon = () => new L.DivIcon({
     popupAnchor: [0, -24],
 });
 
-const createRouteIcon = () => new L.DivIcon({
-    html: '<div style="font-size: 24px; color: #4CAF50;">📍</div>',
+// const createRouteIcon = () => new L.DivIcon({
+//     html: '<div style="font-size: 24px; color: #4CAF50;">📍</div>',
+//     className: '',
+//     iconSize: [24, 24],
+//     iconAnchor: [12, 24],
+//     popupAnchor: [0, -24],
+// });
+
+const createRouteIcon = () => new L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const createUserIcon = () => new L.DivIcon({
+    html: '<div style="font-size: 24px; color: #FF5722;">👤</div>',
     className: '',
     iconSize: [24, 24],
     iconAnchor: [12, 24],
@@ -52,35 +68,54 @@ const ChangeMapCenter = ({ center }) => {
     return null;
 };
 
-const LocationMarker = ({ location, isRouteEndpoint, onClick }) => {
+const getLocationMarkerIcon = (type) => {
+    switch (type) {
+        case 'route':
+            return createRouteIcon();
+        case 'user':
+            return createUserIcon();
+        default:
+            return createQuestionIcon();
+    }
+}
+
+const LocationMarker = ({ location, type, onClick }) => {
     return (
         <Marker
             position={[location.latitude, location.longitude]}
-            icon={isRouteEndpoint ? createRouteIcon() : createQuestionIcon()}
+            icon={getLocationMarkerIcon(type)}
             eventHandlers={{ click: () => onClick(location) }}
         >
             <Popup>
                 <Typography variant="subtitle1">{location.name}</Typography>
                 <Typography variant="body2">{location.description}</Typography>
-                {isRouteEndpoint && <Typography variant="caption">Route endpoint</Typography>}
+                {type === "route" && <Typography variant="caption">Route endpoint</Typography>}
             </Popup>
         </Marker>
     );
 };
 
+const userLocation = { latitude: 50.0745, longitude: 19.9234 };
+
 export default function MapWithRouting() {
+    const [addRouteAllBlocked, setAddRouteAllBlocked] = useState(false);
+
     const [locations] = useState(exampleLocations);
+
     const [mapCenter, setMapCenter] = useState(null);
     const [route, setRoute] = useState(null);
     const [routeEndpoints, setRouteEndpoints] = useState([]);
+    const [fullRoutes, setFullRoutes] = useState(null);
     const mapRef = useRef(null);
 
-    const fetchRoute = async (start, end) => {
+    const fetchRouteBetweenTwoPoints = async (start, end) => {
         try {
             const response = await fetch(
                 `https://router.project-osrm.org/route/v1/driving/${start.longitude},${start.latitude};${end.longitude},${end.latitude}?overview=full&geometries=geojson`
             );
             const data = await response.json();
+
+            console.log(data);
 
             if (data.routes && data.routes.length > 0) {
                 setRoute(data.routes[0].geometry.coordinates.map(coord => [coord[1], coord[0]]));
@@ -92,20 +127,72 @@ export default function MapWithRouting() {
     };
 
     const handleLocationClick = (location) => {
-        if (routeEndpoints.length === 0) {
-            setRouteEndpoints([location]);
-        } else if (routeEndpoints.length === 1) {
-            fetchRoute(routeEndpoints[0], location);
-        } else {
-            setRouteEndpoints([location]);
-            setRoute(null);
-        }
+        console.log("Disabled!");
     };
 
     const clearRoute = () => {
         setRoute(null);
         setRouteEndpoints([]);
     };
+
+    const fetchTripData = async (points) => {
+        console.log("Fetching trip data for points:", points);
+
+        const allPoints = [userLocation, ...points];
+        const requestStr = allPoints.map(point => `${point.longitude},${point.latitude}`).join(';');
+        const tripResponse = await fetch(
+            `https://router.project-osrm.org/trip/v1/driving/${requestStr.slice(0, -1)}?overview=full&geometries=geojson&steps=true&annotations=true`
+        );
+        const tripData = await tripResponse.json();
+
+        console.log(tripData);
+
+        if (tripData.trips && tripData.trips.length > 0) {
+            var routes = [];
+            tripData.trips.forEach(trip => {
+                trip.legs.forEach(leg => {
+                    const geometryFlat = leg.steps.map(step => step.geometry.coordinates.map(coord => [coord[1], coord[0]])).flat();
+                    const geometryPairs = [];
+                    for (let i = 0; i < geometryFlat.length - 1; i++) {
+                        geometryPairs.push([geometryFlat[i], geometryFlat[i + 1]]);
+                    }
+
+                    routes = [...routes, geometryPairs];
+                });
+            });
+
+            // Skip last one, as it is the user location (back to start)
+            setFullRoutes(routes.slice(0, -1));
+            console.log("Fetched routes:", routes);
+        }
+    }
+
+    const addRouteAllHanlder = () => {
+        setAddRouteAllBlocked(true);
+        fetchTripData(exampleLocations);
+        setTimeout(() => {
+            setAddRouteAllBlocked(false);
+        }, 3000); // Reset after 3 seconds
+    }
+
+    const indexToColor = (index) => {
+        const colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#FFA500', '#800080'];
+        var alpha = 1.0;
+        switch (index) {
+            case 0:
+                alpha = 1.0;
+                break;
+            case 1:
+                alpha = 0.7;
+                break;
+            default:
+                alpha = 0.5;
+                break;
+        }
+        var retColor = colors[index % colors.length] + Math.floor(alpha * 255).toString(16).padStart(2, '0');
+        console.log(retColor);
+        return retColor;
+    }
 
     return (
         <Box sx={{ padding: 4 }}>
@@ -121,6 +208,15 @@ export default function MapWithRouting() {
                         Clear Route
                     </Button>
                 )}
+                <Button
+                    disabled={addRouteAllBlocked}
+                    variant="outlined"
+                    color="error"
+                    size="small"
+                    onClick={addRouteAllHanlder}
+                >
+                    Add route to all
+                </Button>
             </Box>
 
             <MapContainer
@@ -141,23 +237,32 @@ export default function MapWithRouting() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
 
+                <LocationMarker
+                    isRouteEndpoint={false}
+                    location = {userLocation}
+                    type = "user"
+                />
+
                 {locations.map((loc) => (
                     <LocationMarker
                         key={loc.id}
                         location={loc}
-                        isRouteEndpoint={routeEndpoints.some(ep => ep.id === loc.id)}
+                        type="route"
                         onClick={handleLocationClick}
                     />
                 ))}
 
-                {route && (
-                    <Polyline
-                        positions={route}
-                        color="#1976d2"
-                        weight={5}
-                        opacity={0.7}
-                    />
-                )}
+                {
+                    fullRoutes && fullRoutes.map((route, index) =>
+                        <Polyline
+                            key = {index}
+                            positions={route}
+                            color={indexToColor(index)}
+                            weight={5}
+                            opacity={0.7}
+                        />
+                    )
+                }
 
                 {mapCenter && <ChangeMapCenter center={mapCenter} />}
             </MapContainer>
